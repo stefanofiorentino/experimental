3ae1e264af4fb9554b81ff27b87363b115989512
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2022-03-08 23:43:07 +0100

    current status

--- test/tdd_demo_start.cpp
+++ test/tdd_demo_start.cpp
@@ -2,7 +2,45 @@
 
 #include <gmock/gmock.h>
 
-TEST(suite, case)
+struct my_stack_t {
+    my_stack_t() : is_empty(true){}
+    int stack[100];
+    bool is_empty;
+    bool empty() const
+    {
+        return is_empty;
+    }
+
+    int pop() 
+    {
+        return -1;
+    }
+
+    bool push(int value)
+    {
+        stack[0] = value;
+        is_empty = false;
+        return true;
+    }
+};
+
+TEST(my_stack_t, empty_true)
 {
-    ASSERT_TRUE(false);
+    my_stack_t s;
+    ASSERT_TRUE(s.empty());
+}
+
+TEST(my_stack_t, empty_pop)
+{
+    my_stack_t s;
+    ASSERT_TRUE(s.empty());
+    ASSERT_EQ(-1, s.pop());
+}
+
+TEST(my_stack_t, empty_push)
+{
+    my_stack_t s;
+    ASSERT_TRUE(s.empty());
+    ASSERT_TRUE(s.push(42));
+    ASSERT_FALSE(s.empty());
 }
