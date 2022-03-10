eb9ff393286519aeb5a067ad5b2eb2992ecf6617
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2022-03-10 19:22:42 +0100

    mocking workshop starting point
    Signed-off-by: Fiorentino, Stefano <stefano.fiorentino@adesso.ch>

--- test/mocking_workshop.cpp
+++ test/mocking_workshop.cpp
@@ -3,25 +3,18 @@
 #include <iostream>
 #include <cstdio>
 
-using ::testing::Return;
-
-struct abstract_remote_interface
+struct remote_interface 
 {
-    virtual bool is_available() const = 0;
-    virtual ~abstract_remote_interface() = default;
-};
-
-struct remote_interface : abstract_remote_interface
-{
-    bool is_available() const override
+    bool is_available() const 
     {
         return false;
     }
 };
 
-bool perform_action(abstract_remote_interface &ari)
+bool perform_action()
 {
-    if (!ari.is_available())
+    remote_interface ri;
+    if (!ri.is_available())
     {
         return false;
     }
@@ -29,33 +22,7 @@ bool perform_action(abstract_remote_interface &ari)
     return true;
 }
 
-bool perform_action()
-{
-    remote_interface ri;
-    return perform_action(ri);
-}
-
 TEST(mocking_workshop, e2e)
 {
     ASSERT_FALSE(perform_action());
 }
-
-struct remote_interface_mock : abstract_remote_interface
-{
-    MOCK_METHOD(bool, is_available, (), (const));
-};
-
-TEST(mocking_workshop, mock_true)
-{
-    remote_interface_mock rim;
-    EXPECT_CALL(rim, is_available).WillOnce(Return(false));
-    ASSERT_FALSE(perform_action(rim));
-}
-
-TEST(mocking_workshop, mock_false)
-{
-    remote_interface_mock rim;
-    EXPECT_CALL(rim, is_available).WillOnce(Return(true));
-    ASSERT_TRUE(perform_action(rim));
-}
-
