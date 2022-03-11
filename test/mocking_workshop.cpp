5ca049e22571512e853cadb75422f4821840b5b4
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2022-03-11 11:18:22 +0100

    snapshot after meeting

--- test/mocking_workshop.cpp
+++ test/mocking_workshop.cpp
@@ -3,17 +3,24 @@
 #include <iostream>
 #include <cstdio>
 
-struct remote_interface 
+using ::testing::Return;
+
+struct abstract_remote_interface
 {
-    bool is_available() const 
+    virtual bool is_available() const = 0;
+    virtual ~abstract_remote_interface() = default;
+};
+
+struct remote_interface : abstract_remote_interface
+{
+    bool is_available() const override
     {
         return false;
     }
 };
 
-bool perform_action()
+bool perform_action(abstract_remote_interface &ri)
 {
-    remote_interface ri;
     if (!ri.is_available())
     {
         return false;
@@ -22,7 +29,57 @@ bool perform_action()
     return true;
 }
 
+bool perform_action()
+{
+    remote_interface ri;
+    return perform_action(ri);
+}
+
 TEST(mocking_workshop, e2e)
 {
     ASSERT_FALSE(perform_action());
+}
+
+TEST(mocking_workshop, e2e_di)
+{
+    remote_interface ri;
+    ASSERT_FALSE(perform_action(ri));
+}
+
+struct remote_interface_mock : abstract_remote_interface
+{
+    MOCK_METHOD(bool, is_available, (), (const));
+};
+
+struct mocking_workshop_fixture : ::testing::Test
+{
+    mocking_workshop_fixture() : rim() {}
+
+    void SetUp() override
+    {
+        // setup your fixture
+        std::puts(__FUNCSIG__);
+    }
+
+    void TearDown() override
+    {
+        // tear down fixture
+        std::puts(__FUNCSIG__);
+    }
+
+    remote_interface_mock rim;
+};
+
+TEST_F(mocking_workshop_fixture, default_mock)
+{
+    std::puts(__FUNCSIG__);
+    EXPECT_CALL(rim, is_available);
+    ASSERT_FALSE(perform_action(rim));
+}
+
+TEST_F(mocking_workshop_fixture, true_mock)
+{
+    std::puts(__FUNCSIG__);
+    EXPECT_CALL(rim, is_available).WillOnce(Return(true));
+    ASSERT_TRUE(perform_action(rim));
 }
