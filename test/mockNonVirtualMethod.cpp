de4fbbceef11d2ed3988fe47f3c368749f2b309a
Your Name <you@example.com>
2025-07-30 16:20:05 +0200

    adding mock Non Virtual Method example
    Signed-off-by: Your Name <you@example.com>

---
+++ test/mockNonVirtualMethod.cpp
@@ -0,0 +1,37 @@
+#include <gmock/gmock.h>
+
+class OriginalClass {
+public:
+    void nonVirtualMethod() {
+        // nothing to do, here
+    }
+};
+
+class IWrapper {
+public:
+    virtual ~IWrapper() = default;
+    virtual void nonVirtualMethod() = 0;
+};
+
+// In your tests, mock IWrapper instead of OriginalClass
+class MockWrapper : public IWrapper {
+public:
+    MOCK_METHOD(void, nonVirtualMethod, (), (override));
+};
+
+void execute(IWrapper& w) {
+  w.nonVirtualMethod();
+}
+
+TEST(mockNonVirtualMethod, simple)
+{
+    // Arrange
+    MockWrapper wrapper;
+    EXPECT_CALL(wrapper, nonVirtualMethod());
+
+    // Act
+    execute(wrapper);
+
+    // Assert
+    // googlemock internals will do the asserts for us.
+}
