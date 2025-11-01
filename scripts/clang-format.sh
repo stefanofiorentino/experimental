5eafaa5e07bba8c9ea309788472c26a6c5b332ed
Stefano Fiorentino <stefano.fiorentino@adesso.ch>
2025-11-01 11:29:01 +0000

    clang-format fix
    Signed-off-by: Stefano Fiorentino <stefano.fiorentino@adesso.ch>

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-find ./test/ -not -type d -not -name "*.txt" | xargs -r -n1 clang-format-14 --fallback-style=Mozilla -i
+git ls-files *.[ch] | xargs -r -n1 clang-format-14 --fallback-style=Mozilla -i
+git ls-files *.[ch]pp | xargs -r -n1 clang-format-14 --fallback-style=Mozilla -i
