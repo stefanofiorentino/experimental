c29bfdc98c8dd4457070f3cb241f2a10b397cbd8
Stefano Fiorentino <stefano.fiorentino@adesso.ch>
2026-07-10 14:11:07 +0200

    feat: add do_switch/do_dimm mutators to dimmable_light_t
    Needed for XML parsing test in runtime_polymorphism to modify
    state via the polymorphic interface.

--- include/light_management/dimmable_light_bulb.hpp
+++ include/light_management/dimmable_light_bulb.hpp
@@ -20,6 +20,14 @@ struct dimmable_light_t final // cppcheck-suppress ctuOneDefinitionRuleViolation
     oss << "</dimmable_light>\n";
   }
 
+  void do_switch(bool is_on) {
+    this->is_on = is_on;
+  }
+
+  void do_dimm(int level) {
+    this->level = level;
+  }
+
 private:
   bool is_on{ true };
   int level{ 42 };
